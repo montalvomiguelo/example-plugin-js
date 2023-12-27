8990503512ef6118997048d7d50a55c0618b8c8d
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-12-27 14:26:01 -0600

    Migrating to @oclif/core from deprecated libraries

---
+++ bin/run.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line unicorn/prefer-top-level-await
+(async () => {
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  const oclif = await import('@oclif/core')
+  await oclif.execute({type: 'esm', dir: import.meta.url})
+})()
